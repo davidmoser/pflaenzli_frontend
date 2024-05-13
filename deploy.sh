41f2deaa9eacce54ef33f16fe019aacf218e8110
David Moser <david.moser@gmx.net>
2024-05-13 17:26:32 +0200

    Redeployment fix

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 npm run build
+sudo rm -rf /var/www/pflaenzli_frontend
 sudo mkdir /var/www/pflaenzli_frontend
 sudo cp -r dist/* /var/www/pflaenzli_frontend
 sudo chown -R www-data:www-data /var/www/pflaenzli_frontend/
